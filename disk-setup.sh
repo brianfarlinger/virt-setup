0032f3de2538e3ea1ea1789550cd2954266f09a3
Brian Farlinger <brian@lilac.red>
2016-08-19 14:13:54 -0700

    Update disk-setup.sh

--- disk-setup.sh
+++ disk-setup.sh
@@ -2,6 +2,9 @@
 
 #run this script on an existing vm to prepare a standard vm template
 
+#shutdown the template vm
+virsh shutdown template-machine
+
 #copy standard image template to a new location. If this image file is attached to a vm, make sure the vm is shutdown.
 cp var/lib/libvirt/images/template-machine.qcow2 /home/kvm/templates/template.qcow2
 
