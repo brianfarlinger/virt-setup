55ace8a28d535d5017225eb5bc49a7f59aa9c005
Brian Farlinger <brian@lilac.red>
2016-08-19 14:01:53 -0700

    Update disk-setup.sh

--- disk-setup.sh
+++ disk-setup.sh
@@ -3,7 +3,7 @@
 #run this script on an existing vm to prepare a standard vm template
 
 #copy standard image template to a new location. If this image file is attached to a vm, make sure the vm is shutdown.
-cp var/lib/libvirt/images/template-machine.qemu /home/kvm/templates/template.qemu
+cp var/lib/libvirt/images/template-machine.qcow2 /home/kvm/templates/template.qcow2
 
 #use virt-sysprep to prepare the image. Uses default operations settings.
-virt-sysprep -a /home/kvm/templates/template.qemu --operations abrt-data, bash-history, crash-data, fs-uuids, kerberos-data, logfiles, lvm-uuids, machine-id, net-hwaddr, yum-uuid
+virt-sysprep -a /home/kvm/templates/template.qcow2 --operations abrt-data, bash-history, crash-data, fs-uuids, kerberos-data, logfiles, lvm-uuids, machine-id, net-hwaddr, yum-uuid
